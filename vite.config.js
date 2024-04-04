dd563721d91c59dd99fee62dfaaf1676a83ea9d3
artem <artembaranov.00@mail.ru>
2024-04-04 16:31:21 +0300

    fixed vite-config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    base: '/project-airlines',
     rollupOptions: {
       output: {
         entryFileNames: `main.js`,
@@ -11,6 +12,5 @@ export default defineConfig({
       },
     }
   },
-  base: '/project-airlines',
   plugins: [vue()],
 })
